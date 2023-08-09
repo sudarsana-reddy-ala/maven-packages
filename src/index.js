e540ca2c5a4e7956cbe20ef5ec68629713875d1d
Sud <sudarsana.ala@gmail.com>
2023-08-09 19:01:29 +0530

    sdfsdfs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const { MySqlUtil } = require('./mysql')
+
+async function testSqlInjection(){
+    console.log("Hello World");
+    const mySqlUtil = new MySqlUtil();
+    const data = await mySqlUtil.getUsers({
+        username: 'test',
+        password: 'test123'
+    })
+
+    console.log(data);
+}
+
--- src/mysql.js
+++ src/mysql.js
@@ -1,11 +1,14 @@
-import mysql from 'mysql2/promise';
+const mysql = require('mysql2/promise');
 
-export default async function getUsers(data) {
+class MySqlUtil {
+    async getUsers(data) {
 
-    const connection = await mysql.createConnection(process.env.DATABASE_URL);
-    txtUserId = getRequestString("UserId");
-    const query = `SELECT * FROM accounts WHERE username = ${data.username} AND password = ${data.password}`;
-    const [rows] = await connection.query(query);
-    return rows;
+        const connection = await mysql.createConnection(process.env.DATABASE_URL);
+        txtUserId = getRequestString("UserId");
+        const query = `SELECT * FROM accounts WHERE username = ${data.username} AND password = ${data.password}`;
+        const [rows] = await connection.query(query);
+        return rows;
+    }
 }
 
+module.exports = { MySqlUtil }
