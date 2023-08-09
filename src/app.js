1a792b211e798c78605a4bd88325788383dd373c
Sud <sudarsana.ala@gmail.com>
2023-08-09 19:40:55 +0530

    fdsfd

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send(`<h1>Hello, ${name}</h1>`);
 });
 
+app.get("/hello", (req, res) => {
+    // Do a resource-intensive operation
+    while (true) {}
+  });
+
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
