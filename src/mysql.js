b060199378e7b4b967fd5a9433e0f8dda8b63fb2
Sud <sudarsana.ala@gmail.com>
2023-08-09 16:47:01 +0530

    updated mysql code

--- src/mysql.js
+++ src/mysql.js
@@ -2,5 +2,6 @@ import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
-const query = "SELECT * FROM Repository WHERE TAG = 'javascript' AND public = 1";
+txtUserId = getRequestString("UserId");
+query = "SELECT * FROM Users WHERE UserId = " + txtUserId;
 const [rows] = await connection.query(query);
