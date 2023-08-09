bc4271038134ea2fb6a3485b222e92aef648015e
Sud <sudarsana.ala@gmail.com>
2023-08-09 18:50:35 +0530

    asdsa

--- src/mysql.js
+++ src/mysql.js
@@ -4,7 +4,7 @@ export default async function getUsers(data) {
 
     const connection = await mysql.createConnection(process.env.DATABASE_URL);
     txtUserId = getRequestString("UserId");
-    const query = `SELECT * FROM health_records WHERE id = (${data.id})`;
+    const query = `SELECT * FROM accounts WHERE username = ${data.username} AND password = ${data.password}`;
     const [rows] = await connection.query(query);
     return rows;
 }
